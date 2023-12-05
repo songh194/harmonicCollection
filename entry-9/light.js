062c725e177a2cad57e4fb622edd4971737f413b
songh194 <songh194@newschool.edu>
2023-12-05 01:23:33 -0500

    entry 9 -- bedroom added and alt tags, z-index problem fixed

---
+++ entry-9/light.js
@@ -0,0 +1,5 @@
+const toggleButton = document.getElementById('toggleButton');
+
+toggleButton.addEventListener('click', function() {
+  document.body.classList.toggle('dark-mode'); // Toggle dark-mode class on body
+});
