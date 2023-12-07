fea2442d3146c28c0b73346b35e48d33de1aca3a
songh194 <songh194@newschool.edu>
2023-12-07 18:04:26 -0500

    borderradius

---
+++ entry-9 copy/bedroom.js
@@ -0,0 +1,86 @@
+
+function openPopup1() {
+    var popup = document.getElementById("myPopup1");
+    popup.style.display = "block";
+}
+
+function openPopup2() {
+    var popup = document.getElementById("myPopup2");
+    popup.style.display = "block";
+}
+function openPopup3() {
+    var popup = document.getElementById("myPopup3");
+    popup.style.display = "block";
+}
+
+function openPopup4() {
+    var popup = document.getElementById("myPopup4");
+    popup.style.display = "block";
+}
+
+function openPopup5() {
+    var popup = document.getElementById("myPopup5");
+    popup.style.display = "block";
+}
+
+function openPopup6() {
+    var popup = document.getElementById("myPopup6");
+    popup.style.display = "block";
+}
+
+function openPopup7() {
+    var popup = document.getElementById("myPopup7");
+    popup.style.display = "block";
+}
+
+function openPopup8() {
+    var popup = document.getElementById("myPopup7");
+    popup.style.display = "block";
+}
+function openPopup9() {
+    var popup = document.getElementById("myPopup9");
+    popup.style.display = "block";
+
+}
+// closing function
+
+
+document.getElementById("closePopup1").addEventListener("click", function() {
+    var popup = document.getElementById("myPopup1");
+    popup.style.display = "none";
+});
+
+
+document.getElementById("closePopup2").addEventListener("click", function() {
+    var popup = document.getElementById("myPopup2");
+    popup.style.display = "none";
+});
+
+document.getElementById("closePopup3").addEventListener("click", function() {
+    var popup = document.getElementById("myPopup3");
+    popup.style.display = "none";
+});
+document.getElementById("closePopup4").addEventListener("click", function() {
+    var popup = document.getElementById("myPopup4");
+    popup.style.display = "none";
+});
+document.getElementById("closePopup5").addEventListener("click", function() {
+    var popup = document.getElementById("myPopup5");
+    popup.style.display = "none";
+});
+document.getElementById("closePopup6").addEventListener("click", function() {
+    var popup = document.getElementById("myPopup6");
+    popup.style.display = "none";
+});
+document.getElementById("closePopup7").addEventListener("click", function() {
+    var popup = document.getElementById("myPopup7");
+    popup.style.display = "none";
+});
+document.getElementById("closePopup8").addEventListener("click", function() {
+    var popup = document.getElementById("myPopup8");
+    popup.style.display = "none";
+});
+document.getElementById("closePopup9").addEventListener("click", function() {
+    var popup = document.getElementById("myPopup9");
+    popup.style.display = "none";
+});
